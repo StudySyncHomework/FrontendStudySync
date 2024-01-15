ef7793a5556ee9270029a7555995dfe90439d949
Sanket Koirala <koiralasanket11@gmail.com>
2024-01-15 16:04:59 -0600

    App component updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListsComponent } from './lists/lists.component';
+import { DescriptionComponent } from './description/description.component';
+import { NewHomeworkComponent } from './new-homework/new-homework.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListsComponent,
+    DescriptionComponent,
+    NewHomeworkComponent, 
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+        HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
