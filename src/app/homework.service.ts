a5555d420634a32cab9684378e3331ba34088158
Sanket Koirala <koiralasanket11@gmail.com>
2024-01-15 15:30:22 -0600

    Homework service

---
+++ src/app/homework.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { WebRequestService } from './web-request.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeworkService {
+
+ constructor(private webReqService: WebRequestService) { }
+
+  createHomework(title: string, status: string, date: string, priority: string, details: string){
+    return this.webReqService.post('homeworks', {title, status, date, priority, details  });
+  }
+
+  getHomeworks(){
+    return this.webReqService.get('homeworks')
+  }
+}
