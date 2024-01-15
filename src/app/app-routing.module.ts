dc273e86ff38551ce1bfbfe15eb1c76438c2627e
Sanket Koirala <koiralasanket11@gmail.com>
2024-01-15 16:04:35 -0600

    Routes Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewHomeworkComponent } from './new-homework/new-homework.component';
+import { ListsComponent } from './lists/lists.component';
+import { DescriptionComponent } from './description/description.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: ListsComponent  },
+  {path: 'new-homework', component: NewHomeworkComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
